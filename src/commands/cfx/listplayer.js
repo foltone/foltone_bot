cd5863a7d031053e3b146a06a20b940ae98d7752
foltone <77021937+foltone@users.noreply.github.com>
2022-10-08 14:14:36 +0200

    debug listplayer

--- src/commands/cfx/listplayer.js
+++ src/commands/cfx/listplayer.js
@@ -1,6 +1,6 @@
 const { EmbedBuilder } = require('discord.js');
 const cfx = require('cfx-api');
-const config = require('../../../config.json')
+const config = require('../../../config.json');
 
 module.exports = {
   name: 'listplayer',
@@ -16,6 +16,11 @@ module.exports = {
 
     if (listPlayer.length > 4096) {
       interaction.reply({ content: 'trop de joueur', ephemeral: true });
+    } else if (!listPlayer.length || listPlayer.length <= 0) {
+      const newEmbed = new EmbedBuilder()
+          .setTitle('Liste des joueurs :')
+          .setDescription(`aucun joueur`);
+      interaction.reply({embeds: [newEmbed]});
     } else {
       interaction.reply({ content: 'listplayer', ephemeral: true });
       const oldEmbed = new EmbedBuilder()
@@ -28,21 +33,21 @@ module.exports = {
         const server = await cfx.fetchServer(config.fivemServer);
         const listPlayer = server.players.map((p) => '[' + p.id + '] ' + p.name ).join(' \n ');
         if (listPlayer.length > 4096) {
-
           const newEmbed = new EmbedBuilder()
               .setTitle('Liste des joueurs :')
               .setDescription(`trop de joueurs`);
           messageHandle.edit({embeds: [newEmbed]});
-
+        } else if (!listPlayer.length || listPlayer.length <= 1) {
+          const newEmbed = new EmbedBuilder()
+              .setTitle('Liste des joueurs :')
+              .setDescription(`aucun joueur`);
+          messageHandle.edit({embeds: [newEmbed]});
         } else {
-
           const newEmbed = new EmbedBuilder()
               .setTitle('Liste des joueurs :')
               .setDescription(`${listPlayer}`);
           messageHandle.edit({embeds: [newEmbed]});
-
         }
-
       }, 15000);
     }
 
